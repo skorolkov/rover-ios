2a5ad87dbfd0be31a39f590fd58405f88452bd80
Sean Rucker <srucker@gmail.com>
2014-10-02 13:55:21 -0400

    Major number for SuperMart

--- Example/Rover App/DebugViewController.m
+++ Example/Rover App/DebugViewController.m
@@ -89,8 +89,8 @@
 
 - (IBAction)simulateButtonPressed:(id)sender
 {
-    NSUUID *UUID = [[NSUUID alloc] initWithUUIDString:@"6C21D507-C7F7-42C5-BA24-ADF3010BC612"];
-    [Rover simulateBeaconWithUUID:UUID major:54378];
+    NSUUID *UUID = [[NSUUID alloc] initWithUUIDString:@"EDA978A7-513C-442A-9364-3F14E74F80EC"];
+    [Rover simulateBeaconWithUUID:UUID major:1];
 }
 
 @end
